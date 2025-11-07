1c8234e0bc3601a94ae567735a28a680948d755d
Tripti Pandey <triptip033@gmail.com>
2025-11-08 02:45:45 +0530

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        accent1: '#38BDF8', // cyan
+        accent2: '#A855F7', // violet
+        accent3: '#22D3EE', // teal
+        panel: '#0f1720'
+      }
+    }
+  },
+  plugins: []
+}
