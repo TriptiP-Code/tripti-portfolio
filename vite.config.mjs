b734325727ea606558e2c7ee5a47dd199e561f7b
Tripti Pandey <triptip033@gmail.com>
2025-11-08 03:41:31 +0530

    Update and rename vite.config.js to vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tripti-portfolio/', // 👈 replace with your repo name
+  base: '/tripti-portfolio/', // use your repo name
 })
