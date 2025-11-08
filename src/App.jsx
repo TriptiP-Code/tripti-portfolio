8b1d1f8f97ba53ec707ebb30276b23d44312e4a5
Tripti Pandey <triptip033@gmail.com>
2025-11-08 13:16:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ export default function App(){
       <div className="container">
         <nav className="flex items-center justify-between py-6">
           <div className="flex items-center gap-4">
-            <img src="/photuuuuuuuuu.jpg" alt="Tripti" className="w-12 h-12 rounded-full object-cover ring-2 ring-slate-700"/>
+            <img src="public/photuuuuuuuuu.jp" alt="Tripti" className="w-12 h-12 rounded-full object-cover ring-2 ring-slate-700"/>
             <div>
               <div className="text-lg font-semibold">Tripti Pandey</div>
               <div className="text-sm text-slate-400">DevOps Engineer</div>
